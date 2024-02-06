6e67208b0e9cf46b8112ff2af8236223f02ba2bb
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-02-06 15:02:36 -0500

    add ref to input field

--- src/components/MainContent/ShoppingList/Item/Item.jsx
+++ src/components/MainContent/ShoppingList/Item/Item.jsx
@@ -1,23 +1,15 @@
 import ItemDisplay from "./ItemDisplay"
 import ItemDisplayRemove from "./ItemDisplayRemove"
 import "./Item.css"
-import { useContext, createContext } from "react"
-import { AppContext } from "../../../../App"
+import { createContext } from "react"
 
 const ItemContext = createContext()
 
 export default function Item({children, itemObj}) {
-  const { checked, id } = itemObj
-
-  const displayItemEl = 
-    checked ? 
-      <ItemDisplayRemove itemID={id}>{children}</ItemDisplayRemove> :
-      <ItemDisplay>{children}</ItemDisplay>
-
+  const { checked } = itemObj
 
   return (
     <ItemContext.Provider value={{itemObj}}>
-      {/* <li className="item--container" onClick={() => updateItem(id, "checked", !checked)}> */}
       <li className="item--container">
         {
           checked ?
--- src/components/MainContent/ShoppingList/Item/ItemDisplayEdit.jsx
+++ src/components/MainContent/ShoppingList/Item/ItemDisplayEdit.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { ItemContext } from "./Item"
 import Form from "../../Form/Form"
 import Button from "../../../Button/Button"
@@ -6,12 +6,17 @@ import Button from "../../../Button/Button"
 
 export default function ItemDisplayEdit() {
   const { itemObj } = useContext(ItemContext)
+  const inputRef = useRef(null)
   const { name } = itemObj
+
+  function handeChange() {
+    console.log(inputRef.current.value)
+  }
 
   return (
     <div className="item--display edit">
       <Form>
-        <input type="text" placeholder={name} />
+        <input type="text" placeholder={name} ref={inputRef} onChange={handeChange} />
       </Form>
     </div>
   )
