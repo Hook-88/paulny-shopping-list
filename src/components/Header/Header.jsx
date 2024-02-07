283bea561ceee8ff1e9e8197512f76d338728ebd
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-02-07 09:03:31 -0500

    improved styles header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import "./Header.css"
+
 export default function Header() {
 
   return (
     <header className="card app--header">
       <h1>Paulny Shopping List</h1>
+      <p className="light-text">Mas descuento!</p>
     </header>
   )
 
