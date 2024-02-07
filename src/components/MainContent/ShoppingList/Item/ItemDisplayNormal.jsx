dd978d07205210a3f568257476b6415e95cc34e0
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-02-06 19:33:03 -0500

    add logic to edit item

--- src/components/MainContent/ShoppingList/Item/ItemDisplayNormal.jsx
+++ src/components/MainContent/ShoppingList/Item/ItemDisplayNormal.jsx
@@ -1,18 +1,30 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useRef } from "react"
 import { AppContext } from "../../../../App"
 import { ItemContext } from "./Item"
 import { FaPenToSquare } from "react-icons/fa6"
 import { FaCheck } from "react-icons/fa6"
+import getCapString from "../../../../utility/getCapString"
 import Button from "../../../Button/Button"
 
 
 export default function ItemDisplayNormal({children}) {
+  const { id, name } = useContext(ItemContext)
   const [onEdit, setOnEdit] = useState(false)
+  const [itemInput, setItemInput] = useState(name)
   const { updateItem } = useContext(AppContext)
-  const { id } = useContext(ItemContext)
+  const inputRef = useRef(null)
 
   function checkItem() {
     updateItem(id, "checked", true)
+  }
+
+  function handleInputChange() {
+    setItemInput(inputRef.current.value)
+  }
+
+  function saveChange() {
+    updateItem(id, "name", itemInput)
+    setOnEdit(false)
   }
 
   return (
@@ -22,8 +34,8 @@ export default function ItemDisplayNormal({children}) {
       <Button className="icon--btn" onClick={() => setOnEdit(true)}><FaPenToSquare /></Button>
     </> : 
     <>
-      <input type="text" />
-      <Button className="icon--btn edit" ><FaCheck /></Button>
+      <input type="text" ref={inputRef} onChange={handleInputChange} value={getCapString(itemInput)}/>
+      <Button className="icon--btn edit" onClick={saveChange}><FaCheck /></Button>
     </>
   )
   
