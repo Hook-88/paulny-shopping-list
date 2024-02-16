87614e42622002b19d41aedbc655da2371eb096a
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-02-16 18:52:29 -0500

    again

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AddItem from "./components/AddItem/AddItem"
+import AppLayout from "./components/AppLayout"
 import "./App.css"
 
 function App() {
 
 
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Header />}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/AddItem/AddItem.jsx
@@ -0,0 +1,11 @@
+import Card from "../Card/Card"
+
+export default function AddItem() {
+
+    return (
+        <Card>
+            <h1>here goes add item</h1>
+        </Card>
+    )
+    
+}
