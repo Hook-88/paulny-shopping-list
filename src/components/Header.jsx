58ca9bda20a9f95574c96acffd956b653d510e54
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-02-16 18:28:38 -0500

    add header and card component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card"
+import "./Header.css"
+
+export default function Header() {
+
+    return (
+        <Card>
+            <header className="shopping-list--header">
+                <h1>Paulny Shopping List</h1>
+            </header>
+        </Card>
+    )
+    
+}
